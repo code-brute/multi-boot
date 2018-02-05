d84fd34029e81097343a12927d82d44d3046f44c
jackson <m18292930341@163.com>
2018-02-05 15:24:10 +0800

    重新构建项目

---
+++ core/src/main/java/com/dingxuan/core/DxCoreApplication.java
@@ -0,0 +1,12 @@
+package com.dingxuan.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxCoreApplication.class, args);
+	}
+}
---
+++ core/src/test/java/com/dingxuan/core/DxCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dingxuan.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DxCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
