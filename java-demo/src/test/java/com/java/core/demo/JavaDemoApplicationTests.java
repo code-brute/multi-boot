32466f71ba986dbcf707db8e93ff458bbb25b18c
jackson <m18292930341@163.com>
2018-06-06 14:07:38 +0800

    。。

--- java-demo/src/test/java/com/java/core/demo/JavaDemoApplicationTests.java
+++ java-demo/src/test/java/com/java/core/demo/JavaDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.java.core.demo;
 
+import com.alibaba.fastjson.JSONObject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
